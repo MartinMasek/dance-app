6dadddd29b052eea5785b4220817d02cd01cc423
MartinMasek <martin@Martins-MacBook-Pro.local>
2019-06-18 12:06:37 +0200

    added video component test

--- src/screens/MoveDetail/index.js
+++ src/screens/MoveDetail/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, SafeAreaView, Text, Dimensions } from 'react-native';
+import { Video } from 'expo-av';
+import { resolveVideoUri } from '../../services/api/VideoUrlResolver';
 
 const VIDEO_WIDTH = Dimensions.get('window').width;
 const VIDEO_HEIGHT = VIDEO_WIDTH * 1.5;
@@ -9,8 +11,20 @@ export default class MoveDetail extends React.Component {
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <View style={{ width: VIDEO_WIDTH, height: VIDEO_HEIGHT, backgroundColor: 'red' }}></View>
-                <Text>MoveDetail</Text>
+                <View style={{ width: VIDEO_WIDTH, height: VIDEO_HEIGHT, backgroundColor: 'red' }}>
+                    <Video
+                        // source={require('../../data/videos/master_lvl_footwork_man.mp4')}
+                        source={resolveVideoUri(0)}
+                        rate={1.0}
+                        volume={1.0}
+                        isMuted={false}
+                        resizeMode="cover"
+                        shouldPlay
+                        isLooping
+                        useNativeControls={true}
+                        style={{ width: VIDEO_WIDTH, height: VIDEO_HEIGHT }}
+                    />
+                </View>
             </SafeAreaView>
         );
     }
---
+++ src/services/api/VideoUrlResolver.js
@@ -0,0 +1,9 @@
+export const resolveVideoUri = (uri) => {
+    return x[uri];
+}
+
+const x = [
+    require('../../data/videos/man_wrap_and_turn.mp4'),
+    require('../../data/videos/master_lvl_footwork_man.mp4'),
+    require('../../data/videos/scisors_woman.mp4')
+]
